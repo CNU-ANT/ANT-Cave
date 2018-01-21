f65c3d9649f76139d9f7dd7aae914715c9da955b
Hyeock Jin <gurwls9628@naver.com>
2018-01-21 20:48:40 +0900

    Signup template과 Form 수정 (#8)
    * 회원가입 template과 form 수정, 팀게시판 모델링

--- ant/forms.py
+++ ant/forms.py
@@ -8,14 +8,101 @@ class LoginForm(forms.Form):
 
 
 class SignupForm(forms.Form):
-    id = forms.CharField(max_length=20, required=True)
-    password = forms.CharField(max_length=20, required=True)
-    email = forms.EmailField(required=True)
-    name = forms.CharField(max_length=10)
-    num = forms.CharField(max_length=10)
-    phone = forms.CharField(max_length=15)
+    id = forms.CharField(
+        max_length=20,
+        label='ID',
+        widget=forms.TextInput(
+            attrs={'class':'form-control',
+                   'placeholder':'아이디를 입력해주세요.',
+                   'required':True,
+        }),
+        required=True,
+    )
+    password = forms.CharField(
+        max_length=20,
+        label='비밀번호',
+        widget=forms.PasswordInput(
+            attrs={'class':'form-control',
+                   'placeholder':'비밀번호를 입력해주세요.',
+                   'required': True,
+
+            }),
+        required=True,
+    )
+    password_confirm = forms.CharField(
+        max_length=20,
+        label='비밀번호 확인',
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control',
+                   'placeholder': '비밀번호를 다시 입력해주세요.',
+                   'required': True,
+
+                   }),
+        required=True,
+    )
+    email = forms.EmailField(
+        label='이메일',
+        widget=forms.EmailInput(
+            attrs={'class':'form-control',
+                   'placeholder':'이메일을 입력해주세요.',
+                   'required':True,
+            }),
+        required=True,
+    )
+    name = forms.CharField(
+        max_length=10,
+        label='이름',
+        widget=forms.TextInput(
+            attrs={'class':'form-control',
+                   'placeholder':'이름을 입력해주세요. ex) 서영학',
+                   'required': True,
+            }),
+        required=True,
+    )
+    num = forms.CharField(
+        max_length=10,
+        label='학번',
+        widget=forms.TextInput(
+            attrs={'class':'form-control',
+                   'placeholder':'201800000 식으로 입력해주세요.',
+                   'minlength': 9,
+                   'maxlength': 9,
+                   'required': True,
+            }),
+        required=True,
+    )
+    phone = forms.CharField(
+        max_length=15,
+        label='전화번호',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder':'- 없이 숫자만 입력해주세요.',
+                'maxlength':15,
+                'minlength':10,
+                'required': True,
+            }),
+        required=True,
+    )
+    is_attend = forms.ChoiceField(
+        label='재학 여부 (시간이 지나면 관리자에 의해 변경될 수 있습니다.)',
+        widget=forms.Select(
+            attrs={
+                'class':'form-control',
+                'style':'width:200px;',
+                'required': True,
+            },
+        ),
+        choices=(
+            ('a', '재학',),
+            ('l', '휴학',),
+            ('g', '졸업',),
+        ),
+        required=True,
+    )
     # class Meta:
     #     model = UserInfo
     #     exclude = [
     #         'user',
     #     ]
+
--- ant/models.py
+++ ant/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -28,8 +29,40 @@ class UserInfo(models.Model):
     user_name = models.CharField(max_length=10)
     user_num = models.IntegerField()
     user_phone = models.CharField(max_length=20)
+    is_attend = models.CharField(max_length=1)
     level = models.PositiveSmallIntegerField(default=0)
-    signup_date = models.CharField(max_length=20)
+    signup_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.user_name.encode('utf8')
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=20)
+    color = models.CharField(max_length=10)
+
+
+class TeamPost(models.Model):
+    post_num = models.AutoField(primary_key=True)
+    parent = models.IntegerField()
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    views = models.IntegerField(default=0)
+
+
+class TeamComment(models.Model):
+    comment_num = models.AutoField(primary_key=True)
+    parent = models.IntegerField()
+    post = models.ForeignKey(TeamPost, primary_key=True, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class TeamLabel(models.Model):
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    post = models.ForeignKey(TeamPost, on_delete=models.CASCADE)
+
+
+class TeamPostFile(models.Model):
+    post = models.ForeignKey(TeamPost, on_delete=models.CASCADE)
+    file = models.FileField()
